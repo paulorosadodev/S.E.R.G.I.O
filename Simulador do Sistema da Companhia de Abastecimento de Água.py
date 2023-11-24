7664c32d215a4450f39eb49869678e8f42f56675
Paulo Rosado <117609505+paulorosadodev@users.noreply.github.com>
2023-11-23 22:21:37 -0300

    Update Simulador do Sistema da Companhia de Abastecimento de Água.py

--- Simulador do Sistema da Companhia de Abastecimento de Água.py
+++ Simulador do Sistema da Companhia de Abastecimento de Água.py
@@ -1,18 +1,21 @@
+#Importa as bibliotecas "serial" e "os"
 import serial
 import os
-os.system('cls')
 
-dados = serial.Serial("COM8", 9600)
+os.system('cls') #Limpa o terminal
 
 lista = []
+
+dados = serial.Serial("COM8", 9600) # Armazena a informação recebida pelo Arduino na variável dados
+ 
 while True:
     if dados.in_waiting:
-        data = dados.readline().decode()
+        data = dados.readline().decode() # Tratamento dos dados
         if "ALERTA" in data:
-            print(data.strip())
+            print(data.strip()) # Printa o alerta de movimentação suspeita
             pass
         else:
-            lista.append(float(data))
+            lista.append(float(data)) 
             consumo_total = sum(lista)
-            conta = consumo_total * 0.25
-            print(f"A consumo do mes foi de {consumo_total:.2f} L, totalizando R${conta:.2f}")
+            conta = consumo_total * 0.25 # Calcula o valor da conta através do consumo total
+            print(f"A consumo do dia foi de {consumo_total:.2f} L, totalizando R${conta:.2f}") # Printa o consumo em L e o valor da conta em R$
